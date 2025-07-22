7d50912eac8731a05a9bd0e09b0bbd0f4f158830
Dragonlol-A <alex.gen1844@gmail.com>
2025-07-22 13:12:32 +0200

    doc update

--- doc/html/menudata.js
+++ doc/html/menudata.js
@@ -33,7 +33,17 @@ var menudata={children:[
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
-{text:"All",url:"globals.html"},
+{text:"All",url:"globals.html",children:[
+{text:"b",url:"globals.html#index_b"},
+{text:"f",url:"globals.html#index_f"},
+{text:"l",url:"globals.html#index_l"},
+{text:"m",url:"globals.html#index_m"},
+{text:"n",url:"globals.html#index_n"},
+{text:"p",url:"globals.html#index_p"},
+{text:"r",url:"globals.html#index_r"},
+{text:"s",url:"globals.html#index_s"},
+{text:"u",url:"globals.html#index_u"},
+{text:"w",url:"globals.html#index_w"}]},
 {text:"Functions",url:"globals_func.html"},
 {text:"Typedefs",url:"globals_type.html"},
 {text:"Enumerations",url:"globals_enum.html"},
--- doc/html/search/all_0.js
+++ doc/html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['black_5fand_5fwhite_0',['black_and_white',['../filter__functions_8c.html#a88f57145200c6b24ee87898d163862a3',1,'filter_functions.c']]]
+  ['black_5fand_5fwhite_0',['black_and_white',['../filter__function_8h.html#a88f57145200c6b24ee87898d163862a3',1,'black_and_white(PNM **image, char *parametre):&#160;filter_functions.c'],['../filter__functions_8c.html#a88f57145200c6b24ee87898d163862a3',1,'black_and_white(PNM **image, char *parametre):&#160;filter_functions.c']]]
 ];
